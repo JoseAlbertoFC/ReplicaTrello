c07823d87a56b30e256d4ebca03ec3c7b312fccf
JoseAlbertoFC <fuenmayorcoa@gmail.com>
2024-01-17 14:17:15 -0300

    Relaciones Creadas, a espera de que Santi las Revise

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/db");
 
 conn.sync({ force : false }).then(() => {
   server.listen(process.env.PORT || 3000, () => {
-    console.log(`Listening on port ${process.env.PORT} / "We are Back Bitches!!!!!!!"`);
+    console.log(`Listening on port ${process.env.PORT}!!! We are Back Bitches!!!!!!!`);
     
   });
 });
