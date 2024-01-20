e0afde942fc0be0116646a832136728a871ebe19
JoseAlbertoFC <fuenmayorcoa@gmail.com>
2024-01-20 00:34:27 -0300

    Ruta PostTask Lista

--- src/controllers/Tasks/createTask.js
+++ src/controllers/Tasks/createTask.js
@@ -1,1 +1,18 @@
-// Acá van los controllers de Tasks
+const { Task } = require("../../../src/db");
+
+const createTask = async (name, description, status) => {
+    try {
+        const newTask = await Task.create({
+            name, description, status
+        })
+
+        return newTask
+    } catch (error) {
+        console.log(error.message)
+        throw new Error(error.message )
+    }
+}
+
+module.exports = {
+    createTask
+}
