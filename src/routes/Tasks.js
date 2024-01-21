53e3627e95c7b521ee713be65d90310009d640e8
JoseAlbertoFC <fuenmayorcoa@gmail.com>
2024-01-20 21:18:58 -0300

    La ruta ReadTasks esta lista y se le hizo un pequeño ajuste a los alias de la base de datos (Se les quito la s para evitar confusiones)

--- src/routes/Tasks.js
+++ src/routes/Tasks.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const { postTask } = require("../handlers/Tasks/postTask");
+const { getTasks } = require("../handlers/Tasks/getTasks");
 
 const tasksRoutes = Router();
 
 tasksRoutes.post("/newTask", postTask);
+tasksRoutes.get("/readTasks", getTasks)
 
 module.exports = { tasksRoutes };
