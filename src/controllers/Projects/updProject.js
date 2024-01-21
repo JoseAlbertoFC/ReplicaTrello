f564a30e67c3e35311dc83e1d6ed1fb0fe5b2071
SH-ur <sdjsm2003@gmail.com>
2024-01-20 21:10:32 -0400

    Update funcionando, éxito!

---
+++ src/controllers/Projects/updProject.js
@@ -0,0 +1,27 @@
+const { Project } = require("../../../src/db");
+
+const updProject = async (info, idToUpd) => {
+  try {
+    const { name, description } = info;
+
+    if (!idToUpd)
+      return "Without Id we cant affort a successful updating operation.";
+    if (!name)
+      return "Sorry, you can't update this Project Reference without a name, because it's necessary for the Database.";
+
+    const updateThatProject = await Project.update(
+      { name: name, description: description },
+      { where: { id: idToUpd } }
+    );
+
+    if (updateThatProject)
+      return (
+        updateThatProject || "Se debió actualizar correctamente, verifiquemos!."
+      );
+      else return 'WOWOWOOWOWOWWOOWOWOOWow... como que algo salió mal.'
+  } catch (error) {
+    throw new Error({ error: error.message });
+  }
+};
+
+module.exports = { updProject };
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -2,10 +2,12 @@
 const {Router} = require('express');
 const { postProject } = require('../handlers/Projects/postProject');
 const {getProjects} = require('../handlers/Projects/getProjects');
+const { updateProjects } = require('../handlers/Projects/updateProjects');
 
 const projectRoutes = Router()
 
 projectRoutes.post('/createProject', postProject)
 .get('/getPro', getProjects)
+.put('/updPro/:id', updateProjects)
 
 module.exports = {projectRoutes};
